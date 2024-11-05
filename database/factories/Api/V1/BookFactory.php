44f1da01114e818e92d73c436a3a5d843e1063bc
Konrad <konasgpro@gmail.com>
2024-11-05 13:21:59 +0100

    feature: Book factory

---
+++ database/factories/Api/V1/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Api\V1;
+
+use App\Models\Api\V1\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name,
+            'release_date' => $this->faker->date,
+            'publishing_house' => $this->faker->company,
+            'is_borrowed' => false,
+            'user_id' => null,
+        ];
+    }
+}
